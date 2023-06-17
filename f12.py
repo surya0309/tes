d7a343d310d7272c566bccb32aec01ff3576d255
surya0309 <modhukurisurya@gmail.com>
2023-06-17 12:02:23 +0530

    modefive

--- f12.py
+++ f12.py
@@ -1,11 +1,11 @@
 # Multiplication table (from 1 to 10) in Python
 
-num = 55
+num = 3
 
 # To take input from the user
 # num = int(input("Display multiplication table of? "))
 
 # Iterate 10 times from i = 1 to 10
-for i in range(1, 11):
+for i in range(1, 15):
    print(num, 'x', i, '=', num*i)
 
